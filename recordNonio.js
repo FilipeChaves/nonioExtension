85438bde064a8b9dd6ad06aa631139a548326cd6
FilipeChaves <filipe_andrebc@hotmail.com>
2019-03-26 11:05:30 +0000

    Update recordNonio.js

--- recordNonio.js
+++ recordNonio.js
@@ -2,7 +2,8 @@ var pageLoadCheck = setInterval(function () {
     if (document.getElementById('layer_gatting')){
 		document.getElementById('layer_gatting').remove()
 		
-		document.body.css({overflow: 'auto'});
+		document.body.style.overflow = 'auto';
+		document.documentElement.style.overflow = 'auto';
 		
 		clearInterval(pageLoadCheck);
 	}
